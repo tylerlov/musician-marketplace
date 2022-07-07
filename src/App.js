40777ddcaea912a3494a774791998b90fc31a181
stylotylo <tylerlovell@gmail.com>
2022-07-07 09:40:09 -0400

    Added Category page as extension to explore page - can click through and see Rent and Sale homes. Currently just shows loading, check database for entires and debug

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Navbar from './components/Navbar';
 import Explore from './pages/Explore';
+import Category from './pages/Category';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
@@ -18,9 +19,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
           <Route path="/profile" element={<PrivateRoute />} >
             <Route path="/profile" element={<Profile />} />
           </Route>
